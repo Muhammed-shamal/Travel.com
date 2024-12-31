24af3eb2aab847150c070d4be12a29efb1203b8d
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-12-31 20:49:47 +0530

    error correction

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,18 +1,8 @@
 export default async function Blog() {
-    const res = await fetch('https://api.example.com/posts', {
-      cache: 'force-cache', // Ensures the content is statically generated at build time
-    });
-    const posts = await res.json();
-  e
-    return (
-      <div>
-        <h1>Blog Posts</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      
+    </div>
+  );
+}
