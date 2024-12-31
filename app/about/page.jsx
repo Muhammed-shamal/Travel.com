afafef3e9f75b496d9314b861e90aa65d667344c
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-12-31 09:41:43 +0530

    about page creation

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative">
+      <div class="mb-16 rounded-full">
+        <h2 class="text-4xl font-manrope font-bold text-gray-900 text-center">
+          What our happy user says!
+        </h2>
+      </div>
+    </div>
+  );
+}
